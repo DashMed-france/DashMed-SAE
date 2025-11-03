bc9105874a5237368c81df580c245b26da7fb811
Eloïse-GIUSIANO[2024-2027] <eloise.giusiano@etu.univ-amu.fr>
2025-11-03 10:20:43 +0100

    amélioration de la liste
    alignement de la liste avec les evenement dans consultations

--- app/controllers/pages/DashboardController.php
+++ app/controllers/pages/DashboardController.php
@@ -3,6 +3,7 @@
 namespace modules\controllers\pages;
 
 use modules\views\pages\dashboardView;
+use modules\models\consultation;
 
 /**
  * Contrôleur du tableau de bord.
@@ -19,8 +20,14 @@ class DashboardController
         if (!$this->isUserLoggedIn())
         {
             header('Location: /?page=login');
+            exit();
         }
-        $view = new dashboardView();
+
+        // Récupérer les consultations
+        $consultations = $this->getConsultations();
+
+        // Passer les consultations à la vue
+        $view = new dashboardView($consultations);
         $view->show();
     }
 
@@ -32,5 +39,50 @@ class DashboardController
     private function isUserLoggedIn(): bool
     {
         return isset($_SESSION['email']);
+    }
+
+    /**
+     * Récupère les consultations du patient.
+     *
+     * @return array
+     */
+    private function getConsultations(): array
+    {
+        // TODO: Remplacer par votre logique de récupération depuis la base de données
+
+        // Exemple avec des données fictives (à remplacer)
+        $consultations = [];
+
+        $consultations[] = new consultation(
+            'Dr. Dupont',
+            '08/10/2025',
+            'Radio du genou',
+            'Résultats normaux',
+            'doc123.pdf'
+        );
+
+        $consultations[] = new consultation(
+            'Dr. Martin',
+            '15/10/2025',
+            'Consultation de suivi',
+            'Patient en bonne voie de guérison',
+            'doc124.pdf'
+        );
+
+        $consultations[] = new consultation(
+            'Dr. Leblanc',
+            '22/10/2025',
+            'Examen sanguin',
+            'Valeurs normales',
+            'doc125.pdf'
+        );
+
+        return $consultations;
+
+        /*
+        // Version avec base de données (exemple)
+        $consultationRepository = new ConsultationRepository();
+        return $consultationRepository->findByPatientEmail($_SESSION['email']);
+        */
     }
 }
