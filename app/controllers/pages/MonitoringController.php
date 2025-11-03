3b120b21cf0de64d99ef09b81473474409f33995
Eloïse-GIUSIANO[2024-2027] <eloise.giusiano@etu.univ-amu.fr>
2025-11-03 16:30:17 +0100

    création de la page vitals

--- app/controllers/pages/MonitoringController.php
+++ app/controllers/pages/MonitoringController.php
@@ -2,7 +2,33 @@
 
 namespace modules\controllers\pages;
 
+use modules\views\pages\monitoringView;
+
 class MonitoringController
 {
+    /**
+     * Affiche la vue du tableau de bord si l'utilisateur est connecté.
+     *
+     * @return void
+     */
+    public function get(): void
+    {
+        if (!$this->isUserLoggedIn())
+        {
+            header('Location: /?page=login');
+        }
+        $view = new monitoringView();
+        $view->show();
+    }
+
+    /**
+     * Vérifie si l'utilisateur est connecté.
+     *
+     * @return bool
+     */
+    private function isUserLoggedIn(): bool
+    {
+        return isset($_SESSION['email']);
+    }
 
 }
