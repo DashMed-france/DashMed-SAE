a92c55aa64b724c98940d1547f4691c96704ef86
PGP_667 <julesribbe@gmail.com>
2025-11-07 14:43:09 +0100

    Create chart.js
    Co-Authored-By: Maxence Torchin <maxence.torchin@etu.univ-amu.fr>

---
+++ public/assets/js/component/modal/chart.js
@@ -0,0 +1,18 @@
+function createChart(type, dataset){
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Chart.js Line Chart'
+                }
+            }
+        },
+    };
+}
