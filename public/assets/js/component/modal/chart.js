3c39187886262713350b185469944a00f9b108d9
PGP_667 <julesribbe@gmail.com>
2025-11-07 18:19:52 +0100

    Integration of charts functionality with fully customizable charts function
    Co-Authored-By: Maxence Torchin <maxence.torchin@etu.univ-amu.fr>

--- public/assets/js/component/modal/chart.js
+++ public/assets/js/component/modal/chart.js
@@ -1,7 +1,40 @@
-function createChart(type, dataset){
+function createChart(
+    type,
+    title = "Titre",
+    labels = [],
+    data = [],
+    target,
+    color = '#275afe'
+) {
+
+    labels = [...labels].reverse();
+    data = [...data].reverse();
+
+    console.log(
+        "type :" + type + "\n",
+        "title :" + title + "\n",
+        "label :" + labels + "\n",
+        "data :" + data + "\n",
+        "target :" + target + "\n",
+        "color :" + color + "\n",
+    )
+    const dataset = {
+        labels: labels,
+        datasets: [{
+            label: title,
+            data: data,
+            borderColor: color,
+            backgroundColor: color + '20',
+            tension: 0.3,
+            fill: false,
+            pointRadius: 5,
+            pointBackgroundColor: color
+        }]
+    };
+
     const config = {
-        type: 'line',
-        data: data,
+        type: type,
+        data: dataset,
         options: {
             responsive: true,
             plugins: {
@@ -9,10 +42,18 @@ function createChart(type, dataset){
                     position: 'top',
                 },
                 title: {
-                    display: true,
-                    text: 'Chart.js Line Chart'
+                    display: false,
+                    text: title
                 }
             }
         },
     };
+
+    const El = document.getElementById(target);
+
+    if (!El) { console.error('Canvas introuvable:', target); return; }
+
+    if (El.chartInstance) El.chartInstance.destroy();
+
+    El.chartInstance = new Chart(El, config);
 }
